f9974dd5b3725b1e3efc9ee79ba440a4727c212a
Ecvasquez <ecvasquez@uninorte.edu.co>
2021-12-18 12:07:48 -0500

    agregando cors

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const userRoutes = require("./routes/users");
 const personRoutes = require("./routes/people");
 const contractRoutes = require("./routes/contracts");
 const applicationRoutes = require("./routes/applications");
-app.use(express.json());
-app.use('/api',userRoutes);
-app.use('/api',personRoutes);
-app.use('/api',contractRoutes);
-app.use('/api',applicationRoutes);
-require('dotenv').config();
 const port = process.env.PORT || 8000;
+const cors_config={
+    origin:'*'
+};
+app.use(express.json());
+app.use('/api',cors(cors_config),userRoutes);
+app.use('/api',cors(cors_config),personRoutes);
+app.use('/api',cors(cors_config),contractRoutes);
+app.use('/api',cors(cors_config),applicationRoutes);
+require('dotenv').config();
+
 //rutas
-app.get('/',(req,res) => {
-    res.send('Bienvenido API Exito');
+app.use('/',cors(cors_config),(req,res) => {
+    //res.send('Bienvenido API Exito');
     res.json({
         mensaje:'prueba'
     });
